419ebd4fd4e509fd09bfd40a473a29f0ef91a345
Amirhossein <sahosseini1382@gmail.com>
2025-10-29 18:37:40 +0330

    Session1

---
+++ Session1/circle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int r = 2;
+
+    float PI = 3.14159265;
+
+
+    float env = 2*PI*r;
+
+
+    printf("env is %f", env);
+
+    return 0;
+}
---
+++ Session1/mmd.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main(){
+
+    // printf("\\Hello World\n");
+    
+    // int a = 10;
+    // // float b = 10.225;
+    // printf("a : %d\t", a);
+    
+    // a+=10; // a= a + 10;
+
+    // a-=20;
+
+    // a+=5;
+
+    // a*=10;
+
+    // a /= 2;
+    // printf("a : %d", a);
+
+
+    // printf("\n%.3f", b);
+
+
+    // char score = 'a';
+
+    // printf("%c\n", score);
+    // printf("%d\n", score);
+
+    /* 
+    dvsvsdfvdf
+    vvsdvsdvs
+    65 => 
+    - - - - - - - -
+    01000001
+    */
+
+
+
+
+
+    int a = 4; // 100
+    int b = 7; // 111
+    int c = 9; // 1001
+    
+    int res1 = a & b;
+    int res2 = a | b;
+    int res3 = a & c;
+
+    // printf("%d\n", res1);
+
+    // printf("%d\n", res2);
+    // printf("%d\n", res3);
+    // printf("%d\n", a & b);
+
+
+    
+    return 0;
+}
+
